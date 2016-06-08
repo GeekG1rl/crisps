362b5820e897f2a0989638f1e52aad4944f24070
Mara Wanot <m.d.wanot@hotmail.com>
2016-06-08 11:01:25 +0100

    trying to make it work

--- lib/crisps.rb
+++ lib/crisps.rb
@@ -3,16 +3,17 @@ require 'rspec/core/formatters/base_text_formatter'
 
 module RSpec
   module Formatter
-  class Crisps < RSpec::Core::Formatters::BaseTextFormatter
+    class Crisps < RSpec::Core::Formatters::BaseTextFormatter
 
-    RSpec::Core::Formatters.register(self, :example_passed, :example_failed)
+      RSpec::Core::Formatters.register(self, :example_passed, :example_failed)
 
-    def example_passed(example)
-      output.print ("chocolate covered crisps - yum!").colorize(:background => :green)
-    end
+      def example_passed(example)
+        output.print ("chocolate covered crisps - yum!").colorize(:background => :green)
+      end
 
-    def example_failed(example)
-      p "burnt toast - boo!"
+      def example_failed(example)
+        p "burnt toast - boo!"
+      end
     end
   end
 end
